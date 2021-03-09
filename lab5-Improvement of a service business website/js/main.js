9f4d86e880d3cf82cbe262a9bb7237676894826f
snehageorge37 <snehageorge37@gmail.com>
2021-03-09 17:52:34 -0500

    Changed input styling to match theme

--- lab5-Improvement of a service business website/js/main.js
+++ lab5-Improvement of a service business website/js/main.js
@@ -1,3 +1,14 @@
-function myFunction() {
-    document.getElementById("myAlert").style.visibility = "visible";
+function myFunction1() {
+    document.getElementById("myAlert").style.display = "block";
+    document.getElementById("closeModal1").click();
+}
+
+function myFunction2() {
+    document.getElementById("myAlert").style.display = "block";
+    document.getElementById("closeModal2").click();
+}
+
+function myFunction3() {
+    document.getElementById("myAlert").style.display = "block";
+    document.getElementById("closeModal3").click();
 }
