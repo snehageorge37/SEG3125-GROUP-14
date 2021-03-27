565dced0944752db729aa96a445d964566e5b136
snehageorge37 <snehageorge37@gmail.com>
2021-03-27 15:58:56 -0400

    Made logo image larger

--- lab7_8_9/starting_point/instarecipeapp/src/NavBar.js
+++ lab7_8_9/starting_point/instarecipeapp/src/NavBar.js
@@ -4,17 +4,15 @@ import Navbar from "react-bootstrap/Navbar";
 class NavBar extends Component {
   render() {
     return (
-      <Navbar bg="dark" variant="dark">
-        <Navbar.Brand href="#home">
-          <img
-            alt=""
-            src="/logo.png"
-            width="30"
-            height="30"
-            className="d-inline-block align-top"
-          />{" "}
-          InstaRecipe
-        </Navbar.Brand>
+      <Navbar bg="light" variant="light">
+        <img
+          alt="InstaRecipe Logo"
+          src="/logo.png"
+          width="135"
+          height="60"
+          className="d-inline-block align-top"
+        />{" "}
+        <Navbar.Brand href="#home">InstaRecipe</Navbar.Brand>
       </Navbar>
     );
   }
