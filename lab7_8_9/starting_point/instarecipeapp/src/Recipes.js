4b9e1b1c1780639fdb08d150a1146930bdb5880e
lamya <labal085@uottawa.ca>
2021-03-30 08:12:18 -0400

    Set up Recipes component

---
+++ lab7_8_9/starting_point/instarecipeapp/src/Recipes.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import NavBar from "./NavBar";
+import Home from "./Home";
+import Ingredients from "./Ingredients";
+
+class Recipes extends Component {
+    render() {
+      return (
+        <div className="home center">
+          <p>recipes page</p>
+        </div>
+      );
+    }
+  }
+  
+  export default Recipes;
