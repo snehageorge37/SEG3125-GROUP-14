16a52e165e5bcab41d98891bebaf88daea4f3781
lamya <labal085@uottawa.ca>
2021-03-31 17:55:04 -0400

    Added recipe image and changed styling of recipe page

--- lab7_8_9/starting_point/instarecipeapp/src/Recipes.js
+++ lab7_8_9/starting_point/instarecipeapp/src/Recipes.js
@@ -1,42 +1,68 @@
 import React, { Component } from "react";
+import banana_smoothie from "./images/banana_smoothie.PNG";
+
+const imgStyle = {
+  border: "1px solid #ddd",
+  borderRadius: "4px",
+  padding: "5px",
+  width: "200px",
+  display: "block",
+  marginLeft: "auto",
+  marginRight: "auto",
+  width: "50%",
+};
+const pStyle = {
+  textAlign: "center",
+  border: "2px solid rgba(0, 0, 0, 0.5)",
+};
+const divBorderStyle = {
+  border: "2px solid rgba(0, 0, 0, 0.5)",
+  marginTop: 30,
+};
 
 class Recipes extends Component {
   render() {
     return (
-      <div class="container">
-        <br />
-        <br />
-        <br />
+      <div class="container" style={divBorderStyle}>
+        <br /><br /><br />
         <div class="row mb-2">
           <div class="col-md-6">
             <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
               <div class="col p-4 d-flex flex-column position-static">
-                <h3 class="mb-0">Recipe title</h3>
+                <h4 class="mb-0" style={pStyle}>Banana Smoothie</h4>
+                <br /><br />
+                <img src={banana_smoothie} alt="banana smoothie" style={imgStyle} />
               </div>
-              <div class="col-auto d-none d-lg-block">
-                <svg
-                  class="bd-placeholder-img"
-                  width="200"
-                  height="250"
-                  xmlns="http://www.w3.org/2000/svg"
-                  role="img"
-                  aria-label="Placeholder: Recipe Image"
-                  preserveAspectRatio="xMidYMid slice"
-                  focusable="false"
-                >
-                  <title>Recipe Image</title>
-                  <rect width="100%" height="100%" fill="#55595c"></rect>
-                  <text x="30%" y="50%" fill="#eceeef" dy=".3em">
-                    Recipe Image
-                  </text>
-                </svg>
-              </div>
+      
+            
             </div>
           </div>
           <div class="col-md-6">
             <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
               <div class="col p-4 d-flex flex-column position-static">
-                <h3 class="mb-0">Nutrition facts</h3>
+                <h4 class="mb-0" style={pStyle}>Nutrition facts</h4>
+                <br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+
+              </div>
+            </div>
+          </div>
+        </div>
+        <br /><br /><br />
+
+        <div class="row mb-2">
+          <div class="col-md-6">
+          <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+              <div class="col p-4 d-flex flex-column position-static">
+                <h4 class="mb-0" style={pStyle}>Ingredients</h4>
+                  <br /><br /><br /><br /><br /><br /><br />
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6">
+          <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+              <div class="col p-4 d-flex flex-column position-static">
+                <h4 class="mb-0" style={pStyle} >Instructions</h4>
+                <br /><br /><br /><br /><br /><br /><br />
               </div>
             </div>
           </div>
